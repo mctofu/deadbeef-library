f615de3ab28b83efc7d21a2bd0ce1de63dd2208e
David McIntosh <804610+mctofu@users.noreply.github.com>
2021-07-19 22:46:59 -0700

    Reference source before libs when building
    Fixes gtk symbol errors on newer Ubuntu versions.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-gcc `pkg-config gtk+-3.0 --cflags` `pkg-config --libs gtk+-3.0` -std=c99 -shared -O2 -g -fPIC -DDEBUG=1 -o ddb_misc_libbrowser_GTK3.so libbrowser.c utils.c support.c client.c $(pwd)/../musiclib-grpc/cgo/build/client.so
+gcc `pkg-config gtk+-3.0 --cflags` libbrowser.c utils.c support.c client.c -std=c99 -shared -O2 -g -fPIC -DDEBUG=1 -o ddb_misc_libbrowser_GTK3.so $(pwd)/../musiclib-grpc/cgo/build/client.so `pkg-config --libs gtk+-3.0`
 
 mkdir -p ~/.local/lib/deadbeef/
 cp ddb_misc_libbrowser_GTK3.so ~/.local/lib/deadbeef/ddb_misc_libbrowser_GTK3.so
